0f53cfd203eea8c99aadc60f28b85823d5b3cc85
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-10-30 22:45:19 +0100

    Add OptionAssert_contains_usingValueComparator_Test

--- src/test/java/org/assertj/vavr/api/OptionAssert_contains_usingValueComparator_Test.java
+++ src/test/java/org/assertj/vavr/api/OptionAssert_contains_usingValueComparator_Test.java
@@ -1,19 +1,84 @@
-/**
+/*
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * <p>
+ *
  * Copyright 2012-2017 the original author or authors.
  */
 package org.assertj.vavr.api;
 
-/**
- * TODO
- */
-public class OptionAssert_contains_usingValueComparator_Test {
+import static org.assertj.core.util.FailureMessages.actualIsNull;
+import static org.assertj.vavr.api.OptionShouldContain.shouldContain;
+import static org.assertj.vavr.api.VavrAssertions.assertThat;
+
+import java.util.Comparator;
+
+import io.vavr.control.Option;
+import org.assertj.vavr.test.BaseTest;
+import org.junit.Test;
+
+public class OptionAssert_contains_usingValueComparator_Test extends BaseTest {
+
+    private static Comparator<Foo> FOO_COMPARATOR = Comparator.comparing(o -> o.getValue().toLowerCase());
+
+    @Test
+    public void should_fail_when_optional_is_null() throws Exception {
+        thrown.expectAssertionError(actualIsNull());
+
+        assertThat((Option<Foo>) null).usingValueComparator(FOO_COMPARATOR).contains(new Foo("something"));
+    }
+
+    @Test
+    public void should_fail_if_expected_value_is_null() throws Exception {
+        thrown.expectIllegalArgumentException("The expected value should not be <null>.");
+
+        assertThat(Option.of(new Foo("something"))).usingValueComparator(FOO_COMPARATOR).contains(null);
+    }
+
+    @Test
+    public void should_pass_if_optional_contains_expected_value() throws Exception {
+        assertThat(Option.of(new Foo("something"))).usingValueComparator(FOO_COMPARATOR).contains(new Foo("SoMething"));
+    }
+
+    @Test
+    public void should_fail_if_optional_does_not_contain_expected_value() throws Exception {
+        Option<Foo> actual = Option.of(new Foo("something"));
+        Foo expectedValue = new Foo("something else");
+
+        thrown.expectAssertionError(shouldContain(actual, expectedValue).create());
+
+        assertThat(actual).usingValueComparator(FOO_COMPARATOR).contains(expectedValue);
+    }
+
+    @Test
+    public void should_fail_if_optional_is_empty() throws Exception {
+        Foo expectedValue = new Foo("test");
+
+        thrown.expectAssertionError(shouldContain(expectedValue).create());
+
+        assertThat(Option.<Foo>none()).usingValueComparator(FOO_COMPARATOR).contains(expectedValue);
+    }
+
+    private static class Foo {
+
+        private final String value;
+
+        public Foo(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "Foo{value='" + value + "'}";
+        }
+    }
 }
