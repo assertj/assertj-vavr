d875d9b78b16191a7d6dad27ddaaa905ab1b135d
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-10-31 13:54:59 +0100

    Add OptionAssert_flatMap_Test

--- src/test/java/org/assertj/vavr/api/OptionAssert_flatMap_Test.java
+++ src/test/java/org/assertj/vavr/api/OptionAssert_flatMap_Test.java
@@ -1,19 +1,47 @@
-/**
+/*
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * <p>
+ *
  * Copyright 2012-2017 the original author or authors.
  */
 package org.assertj.vavr.api;
 
-/**
- * TODO
- */
-public class OptionAssert_flatMap_Test {
+import io.vavr.control.Option;
+import org.assertj.vavr.test.BaseTest;
+import org.junit.Test;
+
+import java.util.function.Function;
+
+import static org.assertj.core.util.FailureMessages.actualIsNull;
+import static org.assertj.vavr.api.VavrAssertions.assertThat;
+
+public class OptionAssert_flatMap_Test extends BaseTest {
+
+    private static final Function<String, Option<String>> UPPER_CASE_OPTIONAL_STRING = s -> (s == null)
+      ? Option.none() : Option.of(s.toUpperCase());
+
+    @Test
+    public void should_fail_when_optional_is_null() {
+        thrown.expectAssertionError(actualIsNull());
+
+        assertThat((Option<String>) null).flatMap(UPPER_CASE_OPTIONAL_STRING);
+    }
+
+    @Test
+    public void should_pass_when_optional_is_empty() {
+        assertThat(Option.<String>none()).flatMap(UPPER_CASE_OPTIONAL_STRING).isEmpty();
+    }
+
+    @Test
+    public void should_pass_when_optional_contains_a_value() {
+        assertThat(Option.of("present")).contains("present")
+          .flatMap(UPPER_CASE_OPTIONAL_STRING)
+          .contains("PRESENT");
+    }
 }
