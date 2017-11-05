3598d43e9a2145a6592d413f4e3c736aecc576af
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-11-05 19:06:06 +0200

    Add OptionAssert_map_Test

--- src/test/java/org/assertj/vavr/api/OptionAssert_map_Test.java
+++ src/test/java/org/assertj/vavr/api/OptionAssert_map_Test.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * <p>
@@ -12,8 +12,35 @@
  */
 package org.assertj.vavr.api;
 
-/**
- * TODO
- */
-public class OptionAssert_map_Test {
+import io.vavr.control.Option;
+import org.assertj.vavr.test.BaseTest;
+import org.junit.Test;
+
+import static io.vavr.API.Some;
+import static org.assertj.core.util.FailureMessages.actualIsNull;
+import static org.assertj.vavr.api.VavrAssertions.assertThat;
+
+public class OptionAssert_map_Test extends BaseTest {
+
+    @Test
+    public void should_fail_when_Option_is_null() {
+        thrown.expectAssertionError(actualIsNull());
+
+        assertThat((Option<String>) null).map(String::length);
+    }
+
+    @Test
+    public void should_pass_when_Option_is_empty() {
+        assertThat(Option.<String> none()).map(String::length).isEmpty();
+    }
+
+    @Test
+    public void should_pass_when_Option_contains_a_value() {
+        assertThat(Option.of("42"))
+          .map(String::length)
+          .contains(2);
+        assertThat(Option.of("42"))
+          .map(s -> null)
+          .isEqualTo(Some(null));
+    }
 }
