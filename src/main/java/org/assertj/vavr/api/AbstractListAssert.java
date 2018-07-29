09766c782682edd2a9673c1e31085445d572e1ff
Michal Chmielarz <michal.chmielarz@gmail.com>
2018-07-29 23:39:15 +0200

    Adds doesNotContain assertion for List

--- src/main/java/org/assertj/vavr/api/AbstractListAssert.java
+++ src/main/java/org/assertj/vavr/api/AbstractListAssert.java
@@ -16,6 +16,7 @@ package org.assertj.vavr.api;
 import io.vavr.collection.List;
 import org.assertj.core.api.AbstractAssert;
 import org.assertj.core.api.AbstractIterableAssert;
+import org.assertj.core.api.IndexedObjectEnumerableAssert;
 import org.assertj.core.data.Index;
 import org.assertj.core.internal.ComparisonStrategy;
 import org.assertj.core.internal.StandardComparisonStrategy;
@@ -23,6 +24,7 @@ import org.assertj.core.internal.StandardComparisonStrategy;
 import static java.lang.String.format;
 import static org.assertj.core.error.ShouldContainAtIndex.shouldContainAtIndex;
 import static org.assertj.core.error.ShouldNotBeEmpty.shouldNotBeEmpty;
+import static org.assertj.core.error.ShouldNotContainAtIndex.shouldNotContainAtIndex;
 import static org.assertj.core.util.Preconditions.checkNotNull;
 
 /**
@@ -35,7 +37,9 @@ import static org.assertj.core.util.Preconditions.checkNotNull;
 abstract class AbstractListAssert<SELF extends AbstractListAssert<SELF, ACTUAL, ELEMENT, ELEMENT_ASSERT>,
         ACTUAL extends List<?extends ELEMENT>,
         ELEMENT,
-        ELEMENT_ASSERT extends AbstractAssert<ELEMENT_ASSERT, ELEMENT>> extends AbstractIterableAssert<SELF, ACTUAL, ELEMENT, ELEMENT_ASSERT> {
+        ELEMENT_ASSERT extends AbstractAssert<ELEMENT_ASSERT, ELEMENT>>
+        extends AbstractIterableAssert<SELF, ACTUAL, ELEMENT, ELEMENT_ASSERT>
+        implements IndexedObjectEnumerableAssert<SELF, ELEMENT> {
 
     private ComparisonStrategy comparisonStrategy;
 
@@ -80,7 +84,49 @@ abstract class AbstractListAssert<SELF extends AbstractListAssert<SELF, ACTUAL, 
 
         Object actualElement = actual.get(index.value);
         if (!comparisonStrategy.areEqual(actualElement, value)) {
-            throwAssertionError(shouldContainAtIndex(actual, value, index, actual.get(index.value), comparisonStrategy));
+            throwAssertionError(shouldContainAtIndex(actual, value, index, actual.get(index.value)));
+        }
+
+        return myself;
+    }
+
+    /**
+     * Verifies that the actual group does not contain the given object at the given index.
+     * <p>
+     * Example:
+     * <pre><code class='java'> List&lt;Ring&gt; elvesRings = List.of(vilya, nenya, narya);
+     *
+     * // assertions will pass
+     * assertThat(elvesRings).contains(vilya, atIndex(1));
+     * assertThat(elvesRings).contains(nenya, atIndex(2));
+     * assertThat(elvesRings).contains(narya, atIndex(0));
+     *
+     * // assertions will fail
+     * assertThat(elvesRings).contains(vilya, atIndex(0));
+     * assertThat(elvesRings).contains(nenya, atIndex(1));
+     * assertThat(elvesRings).contains(narya, atIndex(2));</code></pre>
+     *
+     * @param value the object to look for.
+     * @param index the index where the object should not be stored in the actual group.
+     * @return this assertion object.
+     */
+    public SELF doesNotContain(ELEMENT value, Index index) {
+        isNotNull();
+
+        if (actual.isEmpty()) {
+            throwAssertionError(shouldNotBeEmpty());
+        }
+
+        checkNotNull(index, "Index should not be null");
+        final int maximum = actual.size() - 1;
+        if (index.value > maximum) {
+            String errorMessage = "Index should be between <0> and <%d> (inclusive) but was:%n <%d>";
+            throw new IndexOutOfBoundsException(format(errorMessage, maximum, index.value));
+        }
+
+        Object actualElement = actual.get(index.value);
+        if (comparisonStrategy.areEqual(actualElement, value)) {
+            throwAssertionError(shouldNotContainAtIndex(actual, value, index));
         }
 
         return myself;
