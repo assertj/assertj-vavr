298014c3a26d9f13cf9e28b9b3d3cdf49879ac1b
Michal Chmielarz <michal.chmielarz@gmail.com>
2019-02-13 22:34:36 +0100

    Adds MapAssert.isEmpty tests

---
+++ src/test/java/org/assertj/vavr/api/MapAssert_isEmpty_Test.java
@@ -0,0 +1,47 @@
+package org.assertj.vavr.api;
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * <p>
+ * Copyright 2012-2019 the original author or authors.
+ */
+
+import io.vavr.collection.HashMap;
+import io.vavr.collection.Map;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.error.ShouldBeEmpty.shouldBeEmpty;
+import static org.assertj.core.util.FailureMessages.actualIsNull;
+import static org.assertj.vavr.api.VavrAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MapAssert_isEmpty_Test {
+
+    @Test
+    void should_pass_if_Map_is_empty() {
+        assertThat(HashMap.empty()).isEmpty();
+    }
+
+    @Test
+    void should_fail_when_Map_is_null() {
+        assertThrows(AssertionError.class,
+                () -> assertThat((Map<String, String>) null).isEmpty(),
+                actualIsNull());
+    }
+
+    @Test
+    void should_fail_if_Map_is_not_empty() {
+        final Map<String, String> actual = HashMap.of("key", "value");
+
+        assertThrows(AssertionError.class,
+                () -> assertThat(actual).isEmpty(),
+                shouldBeEmpty(actual).create());
+    }
+}
