0b979ccbb94fd799308ee62b3b31ac8116193337
mchmielarz <michal.chmielarz@gmail.com>
2019-03-06 21:53:16 +0100

    Fixes #36

--- src/main/java/org/assertj/vavr/api/AbstractSeqAssert.java
+++ src/main/java/org/assertj/vavr/api/AbstractSeqAssert.java
@@ -10,7 +10,7 @@ package org.assertj.vavr.api;
   an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
   specific language governing permissions and limitations under the License.
   <p>
-  Copyright 2012-2018 the original author or authors.
+  Copyright 2012-2019 the original author or authors.
  */
 
 import io.vavr.collection.Seq;
